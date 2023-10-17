0e11e8df8c6e7af9619a3a8e363360f9d2c1b892
kinjal <kinjaldoshi1398@gmail.com>
2023-10-17 16:16:20 -0400

    Login page UI done

---
+++ src/pages/Login.jsx
@@ -0,0 +1,61 @@
+import React, { useState } from 'react';
+import '../css/Login.css'; // Import your CSS file for styling
+
+function Login() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    if (name === 'username') {
+      setUsername(value);
+    } else if (name === 'password') {
+      setPassword(value);
+    }
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    
+  };
+
+  const handleForgotPassword = () => {
+    
+  };
+
+  return (
+    <div className="login-container">
+      <form onSubmit={handleSubmit} className="login-form">
+        <h2>Login</h2>
+        <div className="input-group">
+          <label htmlFor="username">Username</label>
+          <input
+            type="text"
+            id="username"
+            name="username"
+            value={username}
+            onChange={handleInputChange}
+            required
+          />
+        </div>
+        <div className="input-group">
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            value={password}
+            onChange={handleInputChange}
+            required
+          />
+        </div>
+        <button type="submit">Login</button>
+        <div className="forgot-password">
+          <a href="#forgot-password" onClick={handleForgotPassword}>Forgot Password?</a>
+        </div>
+      </form>
+    </div>
+  );
+}
+
+export default Login;
