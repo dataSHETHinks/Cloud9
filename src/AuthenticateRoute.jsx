d541fcf9c039a996fe97581c24c7fb48ea92e4c5
kinjal <kinjaldoshi1398@gmail.com>
2023-10-19 20:48:27 -0400

    WIP - upload file api integration

--- src/AuthenticateRoute.jsx
+++ src/AuthenticateRoute.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import { Navigate, useNavigate } from 'react-router-dom'
 
 const AuthenticateRoute = ({ children }) => {
-    const navigate = useNavigate()
     const token = localStorage.getItem("accessToken")
     return token ? children : <Navigate to='/login' />
 }
