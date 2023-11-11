5d82c8cfb304b9dd2ff0b610ceab666ab45e5ddc
Chintal Raval <chintaldraval@gmail.com>
2023-11-10 23:27:01 -0500

    removed redundant code

--- src/components/DashBoardHeader.jsx
+++ src/components/DashBoardHeader.jsx
@@ -1,47 +1,3 @@
-// import React from "react";
-
-// const DashBoardHeader = ({
-//   isSidebarOpen,
-//   toggleSidebar,
-//   profileImage,
-//   toggleDropdown,
-//   isDropdownOpen,
-//   handleProfileDropdownClose,
-//   handleRedirectToChangePassword,
-//   handleLogout,
-//   profileDropdownRef,
-// }) => {
-//   return (
-//     <div className="dashboard-header">
-//       <div className="hamburger-icon mb-4" onClick={toggleSidebar}>
-//         <i className={`fas fa-bars ${isSidebarOpen ? "hidden" : ""}`}></i>
-//       </div>
-//       <div className="spacer"></div>{" "}
-//       <div className="profile mt-5" ref={profileDropdownRef}>
-//         <img
-//           className="profile"
-//           src={profileImage}
-//           alt="Profile"
-//           onClick={toggleDropdown}
-//         />
-//         <div
-//           className={`pro-dropdown-menu ${isDropdownOpen ? "open" : ""}`}
-//           onClick={handleProfileDropdownClose}
-//         >
-//           <ul>
-//             <li onClick={handleRedirectToChangePassword}>Change Password</li>
-//             <li onClick={handleLogout}>Logout</li>
-//           </ul>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default DashBoardHeader;
-
-// DashBoardHeader.jsx
-
 import React, { useState, useRef, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
@@ -65,10 +21,6 @@ const DashBoardHeader = ({ isSidebarOpen, toggleSidebar, profileImage }) => {
       document.removeEventListener("click", handleClickOutside);
     };
   }, []);
-
-  const handleToggleDropdown = () => {
-    setIsDropdownOpen(!isDropdownOpen);
-  };
 
   const handleProfileDropdownClose = () => {
     setIsDropdownOpen(false);
