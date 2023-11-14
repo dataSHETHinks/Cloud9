4d283cde45a5db0c2bb29d4b157209b6796c4f5d
jhanviVadher29 <jhanvivadher10@gmail.com>
2023-11-14 15:21:14 -0500

    Category CRUD working. Antd Remaining

--- src/pages/CategoriesPage.jsx
+++ src/pages/CategoriesPage.jsx
@@ -6,26 +6,46 @@ import { useNavigate } from "react-router-dom";
 import { useEffect, useState } from "react";
 import "../css/CategoriesComponentsCss/CategoryPageCss.css";
 
-import api from "../../apiConfig";
+import api from '../apiConfig';
 
 const CategoriesPage = (params) => {
+  const [allCategory, setAllCategory] = useState([]);
   const navigate = useNavigate();
+  const [newCategoryName, setNewCategoryName] = useState(""); // Step 1
 
   const [isSidebarOpen, setIsSidebarOpen] = useState(true);
   const toggleSidebar = () => {
     setIsSidebarOpen(!isSidebarOpen);
   };
-  const get_file_categories = () =>{
+
+  const handleNewCategoryChange = (e) => {
+    setNewCategoryName(e.target.value); // Step 3
+  };
+
+  const addNewCategory = () => {
+    api("POST", "/data/add_new_category/", { name: newCategoryName })
+      .then((response) => {
+        console.log("New category added:", response.data);
+        setAllCategory((prevCategories) => [...prevCategories, response.data]); // Step 6
+        setNewCategoryName(""); // Clear the input field
+      })
+      .catch((error) => {
+        console.error("Error adding new category:", error);
+      });
+  };
+
+
+  const getFileCategories = () =>{
     api("GET", "/data/get_file_categories/", {})
     .then((response) => {
-      setAllFiles(response.data.data);
+      setAllCategory(response.data.data);
     })
     .catch((error) => {
       console.error("GET Request Error:",error);
     });
   };
   useEffect(() => {
-    get_file_categories();
+    getFileCategories();
   }, []);
 
   return (
@@ -41,7 +61,15 @@ const CategoriesPage = (params) => {
           />
           <div className="category-page-div">
             <div className="child-1">
-              <h1>This is Categories Page.</h1>
+              <div>
+                <input
+                  type="text"
+                  placeholder="New Category Name"
+                  value={newCategoryName}
+                  onChange={handleNewCategoryChange}
+                />
+                <button onClick={addNewCategory}>Add Category</button>
+              </div>
             </div>
             <div className="child-2">
             <div
@@ -52,19 +80,15 @@ const CategoriesPage = (params) => {
         <thead>
           <tr>
             <th>#</th>
-            <th>Category</th>
-            <th>Name</th>
+            <th>Category Name</th>
+            {/* <th>Name</th> */}
           </tr>
         </thead>
         <tbody>
           {allCategory.map((category, index) => (
             <tr key={category.id}>
               <td>{index + 1}</td>
-              <td>{category.title}</td>
-              <td>{category.category_name}</td>
-              <td>{category.uploaded_by_username}</td>
-              <td>{category.modified_at}</td>
-              <td>{category.module_name}</td>
+              <td>{category.name}</td>
             </tr>
           ))}
         </tbody>
