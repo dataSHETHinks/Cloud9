345ddd9af8c85be2bf7058818fff6eadebf6c397
jhanviVadher29 <jhanvivadher10@gmail.com>
2023-11-14 02:13:48 -0500

    category api implementation

--- src/pages/CategoriesPage.jsx
+++ src/pages/CategoriesPage.jsx
@@ -3,8 +3,10 @@ import BaseNav from "../components/base_nav";
 import DashBoardHeader from "../components/DashBoardHeader";
 
 import { useNavigate } from "react-router-dom";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "../css/CategoriesComponentsCss/CategoryPageCss.css";
+
+import api from "../../apiConfig";
 
 const CategoriesPage = (params) => {
   const navigate = useNavigate();
@@ -13,6 +15,18 @@ const CategoriesPage = (params) => {
   const toggleSidebar = () => {
     setIsSidebarOpen(!isSidebarOpen);
   };
+  const get_file_categories = () =>{
+    api("GET", "/data/get_file_categories/", {})
+    .then((response) => {
+      setAllFiles(response.data.data);
+    })
+    .catch((error) => {
+      console.error("GET Request Error:",error);
+    });
+  };
+  useEffect(() => {
+    get_file_categories();
+  }, []);
 
   return (
     <>
@@ -29,7 +43,34 @@ const CategoriesPage = (params) => {
             <div className="child-1">
               <h1>This is Categories Page.</h1>
             </div>
-            <div className="child-2"></div>
+            <div className="child-2">
+            <div
+      className="table-responsive"
+      style={{ maxHeight: "85%", overflowY: "auto" }}
+    >
+      <table className="table table-bordered table-striped">
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Category</th>
+            <th>Name</th>
+          </tr>
+        </thead>
+        <tbody>
+          {allCategory.map((category, index) => (
+            <tr key={category.id}>
+              <td>{index + 1}</td>
+              <td>{category.title}</td>
+              <td>{category.category_name}</td>
+              <td>{category.uploaded_by_username}</td>
+              <td>{category.modified_at}</td>
+              <td>{category.module_name}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+            </div>
           </div>
         </main>
       </div>
