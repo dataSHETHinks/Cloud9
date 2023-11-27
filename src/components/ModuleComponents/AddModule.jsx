cf2911e71a142732b45b7987214a05f51e35cedd
srujanoza <sonuoza24@gmail.com>
2023-11-27 17:29:10 -0500

    UI of Modal fixed

--- src/components/ModuleComponents/AddModule.jsx
+++ src/components/ModuleComponents/AddModule.jsx
@@ -55,6 +55,14 @@ const AddModule = () => {
                 open={visible}
                 onOk={handleOk}
                 onCancel={handleCancel}
+                footer={[
+                    <Button key="cancel" onClick={handleCancel}>
+                      Cancel
+                    </Button>,
+                    <Button key="submit" type="primary" onClick={handleOk} style={{ marginInlineStart: 0 }}>
+                      Submit
+                    </Button>,
+                ]}
             >
                 <Form form={form}>
                     <Form.Item
